14314dd1c171db982ac49e05282126ccb607d398
Dimas Ari <Inchidi@yahoo.com>
2017-08-23 18:05:15 +0800

    Update production.py

--- src/settings/production.py
+++ src/settings/production.py
@@ -2,7 +2,7 @@ from src.settings.base import *
 import dj_database_url
 
 SECRET_KEY = "{{ secret_key }}"
-DEBUG = True
+DEBUG = False
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = []
 DATABASES = {'default': dj_database_url.config()}
