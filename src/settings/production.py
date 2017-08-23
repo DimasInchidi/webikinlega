36d7aa2bee4d2f970c4f702bb1cfc6fff9d370a7
Dimas Ari <Inchidi@yahoo.com>
2017-08-23 18:03:35 +0800

    Update production.py

--- src/settings/production.py
+++ src/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 SECRET_KEY = "{{ secret_key }}"
 DEBUG = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['onlinelega.herokuapp.com']
+ALLOWED_HOSTS = ['onlinelega.herokuapp.com', 'webikinle.ga']
 DATABASES = {'default': dj_database_url.config()}
 DATABASES['default']['CONN_MAX_AGE'] = 500
 AUTH_PASSWORD_VALIDATORS = [
